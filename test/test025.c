9a05651eb2a2b561f82877436eeae499351f333c
Patrick Hennig <patrick_hennig@fastmail.com>
2024-04-23 14:32:33 +0200

    added switch case test

---
+++ test/test025.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(int argc, const char**argv){
+	if(argc<0)return-1;
+	switch(argc){
+		default:
+			{
+				char*progname=*argv;
+				printf("progname is %s",progname);
+				for(int ac=0;ac<argc;ac++){
+					printf("arg %d is %s",ac,argv[ac]);
+				}
+			}
+			break;
+		case 0:
+			break;
+		case 1:
+			{
+				char*progname=*argv;
+				printf("progname is %s",progname);
+			}
+			break;
+		case 2:
+			{
+				char*progname=*argv;
+				printf("progname is %s",progname);
+				printf("arg 1 is %s",argv[1]);
+			}
+			break;
+	}
+	
+	return 0;
+}
+
+// TESTGOAL dereference a pointer
