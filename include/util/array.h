f9bf3322e442a6172ed0af771293bedac811353f
slaide <patrick_hennig@fastmail.com>
2024-05-11 21:38:38 +0100

    add minor documentation to array functions

--- include/util/array.h
+++ include/util/array.h
@@ -17,8 +17,10 @@ enum ARRAY_APPEND_RESULT{
     ARRAY_APPEND_OK=0,
     ARRAY_APPEND_ALLOC_FAIL,
 };
+/* append element pointed to by elem to array */
 enum ARRAY_APPEND_RESULT array_append(array*a,void*elem);
 
 void array_pop_front(array*a);
 void array_pop_back(array*a);
+/* get address of element at index, only valid until next array mutation! */
 void* array_get(array*a,int index);
