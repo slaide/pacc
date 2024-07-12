33b2f5bbe6947f8b4c3ce00272d393882e2db22f
slaide <patrick_hennig@fastmail.com>
2024-07-12 15:19:25 +0200

    added a test for leftover tokens at end of file

--- test.py
+++ test.py
@@ -226,6 +226,7 @@ TEST_FILES=[
 
     Test(file="test/test064.c", level=TestLevel.PARSE, goal="function argument has incompatible type", should_fail=True),
     Test(file="test/test065.c", level=TestLevel.PARSE, goal="accessing non-existent field on a struct", should_fail=True),
+    Test(file="test/test066.c", level=TestLevel.PARSE, goal="leftover tokens at end of file", should_fail=True),
 ]
 
 tests=[
